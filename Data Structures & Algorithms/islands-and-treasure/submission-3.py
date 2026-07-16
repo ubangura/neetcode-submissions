a70558ca3682d9a03ab43d9155e4f0a5d322d62a
Unisa Bangura <110050333+ubangura@users.noreply.github.com>
2026-07-16 10:35:01 -0400

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        TREASURE, LAND = 0, 2147483647
+        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        # Iterate matrix
+            # Append cells of treasure to queue
+        queue = deque()
+
+        for x in range(ROWS):
+            for y in range(COLS):
+                if grid[x][y] == TREASURE:
+                    queue.append((x, y, 0))
+        
+        # Multi-source BFS
+        while queue:
+            level = len(queue)
+
+            for cell in range(level):
+                x, y, dist = queue.popleft()
+
+                for dx, dy in DIRECTIONS:
+                    new_x , new_y = x + dx, y + dy
+
+                    if (0 <= new_x < ROWS
+                    and 0 <= new_y < COLS
+                    and grid[new_x][new_y] == LAND):
+                        grid[new_x][new_y] = dist + 1
+                        queue.append((new_x, new_y, dist + 1))
+            
+        
